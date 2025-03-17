da9bed39a5f70aa48457015fba71c754ba98756b
Ian <iancunha20@gmail.com>
2025-03-17 00:02:26 -0300

    MSN POO
    - Encapsulamento
    - Herança
    - Abstração
    - Polimorfismo

---
+++ src/msn/MSNMenssenger.java
@@ -0,0 +1,19 @@
+package msn;
+
+public class MSNMenssenger {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando conexão com a internet");
+    }
+
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando histórico de mensagens");
+    }
+}
