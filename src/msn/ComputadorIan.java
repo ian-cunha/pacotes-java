dabc97f7edf51354206df16765da76ad9d3098fc
Ian <iancunha20@gmail.com>
2025-03-17 15:44:44 -0300

    Encapsulamento

---
+++ src/msn/ComputadorIan.java
@@ -0,0 +1,10 @@
+package msn;
+
+public class ComputadorIan {
+    public static void main(String[] args) {
+        MSNMenssenger msn = new MSNMenssenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
--- src/msn/MSNMenssenger.java
+++ src/msn/MSNMenssenger.java
@@ -2,18 +2,20 @@ package msn;
 
 public class MSNMenssenger {
     public void enviarMensagem() {
+        validarConectadoInternet();
         System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
     }
 
     public void receberMensagem() {
         System.out.println("Recebendo mensagem");
     }
 
-    public void validarConectadoInternet() {
+    private void validarConectadoInternet() {
         System.out.println("Validando conexão com a internet");
     }
 
-    public void salvarHistoricoMensagem() {
+    private void salvarHistoricoMensagem() {
         System.out.println("Salvando histórico de mensagens");
     }
 }
